01f21f1b1bed48a31ec5c9469fd33de0f7d555ac
sql <teshyfraud@gmail.com>
2026-03-06 15:04:22 -0500

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,32 @@
+const CACHE="nebula-cache-v1"
+
+const ASSETS=[
+
+"/",
+"/index.html",
+"/style.css",
+"/os.js",
+"/fs.js",
+"/apps.json"
+
+]
+
+self.addEventListener("install",e=>{
+
+e.waitUntil(
+
+caches.open(CACHE).then(c=>c.addAll(ASSETS))
+
+)
+
+})
+
+self.addEventListener("fetch",e=>{
+
+e.respondWith(
+
+caches.match(e.request).then(r=>r||fetch(e.request))
+
+)
+
+})
