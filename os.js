1d553a3734a4e88404fab00b9b53552305f025c8
sql <teshyfraud@gmail.com>
2026-03-06 15:09:24 -0500

    Update os.js

--- os.js
+++ os.js
@@ -9,30 +9,31 @@ async function init(){
 
 registerServiceWorker()
 
-let data = await fetch("apps.json").then(r=>r.json())
+let registry = await fetch("apps/_registry.json").then(r=>r.json())
 
-for(let app of data.apps){
+for(let app of registry.apps){
 
-loadApp(app)
-
-}
+registerApp(app)
 
 }
 
-async function loadApp(app){
+}
 
-let manifest = await fetch(`apps/${app}.app/manifest.json`).then(r=>r.json())
+async function registerApp(app){
 
-let btn=document.createElement("div")
+let manifest = await fetch(`apps/${app}.app/manifest.json`)
+.then(r=>r.json())
+
+let btn = document.createElement("div")
 btn.className="taskbar-item"
-btn.title=manifest.name
+btn.title = manifest.name
 
-let img=document.createElement("img")
-img.src=`apps/${app}.app/${manifest.icon}`
+let img = document.createElement("img")
+img.src = `apps/${app}.app/${manifest.icon}`
 
 btn.appendChild(img)
 
-btn.onclick=()=>launchApp(app,manifest)
+btn.onclick = () => launchApp(app,manifest)
 
 taskbar.appendChild(btn)
 
@@ -40,44 +41,58 @@ taskbar.appendChild(btn)
 
 function launchApp(app,manifest){
 
-let win=document.createElement("div")
+let win = createWindow(manifest.name)
+
+let iframe = document.createElement("iframe")
+iframe.src = `apps/${app}.app/index.html`
+
+win.appendChild(iframe)
+
+}
+
+function createWindow(titleText){
+
+let win = document.createElement("div")
 win.className="window"
 
-win.style.width="500px"
-win.style.height="350px"
+win.style.width="600px"
+win.style.height="400px"
 win.style.left="200px"
-win.style.top="150px"
+win.style.top="120px"
 
 focusWindow(win)
 
-let title=document.createElement("div")
-title.className="titlebar"
+let titlebar=document.createElement("div")
+titlebar.className="titlebar"
 
-title.innerHTML=`
-<span>${manifest.name}</span>
-
+titlebar.innerHTML=`
+<span>${titleText}</span>
 <div class="window-controls">
-
 <span data-act="min">_</span>
 <span data-act="max">□</span>
 <span data-act="close">✕</span>
-
 </div>
 `
 
-let iframe=document.createElement("iframe")
-
-// lazy loading
-iframe.dataset.src=`apps/${app}.app/index.html`
-
-win.appendChild(title)
-win.appendChild(iframe)
+win.appendChild(titlebar)
 
 windows.appendChild(win)
 
-iframe.src=iframe.dataset.src
+setupControls(win,titlebar)
+makeDraggable(win,titlebar)
 
-makeDraggable(win,title)
+return win
+
+}
+
+function focusWindow(win){
+
+z++
+win.style.zIndex=z
+
+}
+
+function setupControls(win,title){
 
 title.querySelectorAll("span").forEach(btn=>{
 
@@ -102,16 +117,6 @@ win.style.height="calc(100% - 40px)"
 
 })
 
-win.onclick=()=>focusWindow(win)
-
-}
-
-function focusWindow(win){
-
-z++
-
-win.style.zIndex=z
-
 }
 
 function makeDraggable(win,bar){
@@ -121,18 +126,14 @@ let ox=0
 let oy=0
 
 bar.onmousedown=e=>{
-
 drag=true
-
 ox=e.clientX-win.offsetLeft
 oy=e.clientY-win.offsetTop
-
 }
 
 document.onmouseup=()=>drag=false
 
 document.onmousemove=e=>{
-
 if(!drag) return
 
 win.style.left=e.clientX-ox+"px"
